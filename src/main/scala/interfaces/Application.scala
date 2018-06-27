a62364232ebe558059acedf17f5866884bdcec44
Hiroshi Sugawara <>
2018-06-27 22:00:11 +0900

    feat: RSS を取得してみる

--- src/main/scala/interfaces/Application.scala
+++ src/main/scala/interfaces/Application.scala
@@ -1,5 +1,10 @@
 package interfaces
 
+import com.softwaremill.sttp._
+
 object Application extends App {
-  println("hello")
+  val request = sttp.get(uri"http://b.hatena.ne.jp/hotentry/all.rss")
+  implicit val backend: SttpBackend[Id, Nothing] = HttpURLConnectionBackend()
+  val response = request.send()
+  println(response)
 }
