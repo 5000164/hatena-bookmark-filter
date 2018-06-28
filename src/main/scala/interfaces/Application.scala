bc367e3b45dc1187f4655eda9f9c95aba2375c54
Hiroshi Sugawara <5000164@users.noreply.github.com>
2018-06-28 23:37:30 +0900

    feat: 記事の URL 一覧の要素まで絞り込む

--- src/main/scala/interfaces/Application.scala
+++ src/main/scala/interfaces/Application.scala
@@ -2,9 +2,13 @@ package interfaces
 
 import com.softwaremill.sttp._
 
+import scala.xml.XML
+
 object Application extends App {
   val request = sttp.get(uri"http://b.hatena.ne.jp/hotentry/all.rss")
   implicit val backend: SttpBackend[Id, Nothing] = HttpURLConnectionBackend()
   val response = request.send()
-  println(response)
+  val xmlString = response.body.getOrElse("")
+  val xml = XML.loadString(xmlString)
+  (xml \ "channel" \ "items" \ "Seq" \ "li").foreach(println)
 }
