03f0418b81c3def5c3ae3e8edc61c0be94060ff5
Hiroshi Sugawara <5000164@users.noreply.github.com>
2018-07-01 22:49:27 +0900

    feat: 配信されたページのはてなブックマーク数も一緒に投稿する

--- src/main/scala/interfaces/Application.scala
+++ src/main/scala/interfaces/Application.scala
@@ -8,8 +8,8 @@ object Application extends App {
   val executedAt = new Date
   val lastExecutedAt = Recorder.getLastExecutedAt
 
-  val feedUrlList = Feeder.fetchUrlList(settings.feedUrl, settings.threshold, lastExecutedAt)
-  Slack.post(settings.slackToken, settings.slackPostChannelId, settings.slackUserName, settings.slackIconEmoji, feedUrlList)
+  val pageList = Feeder.fetchPageList(settings.feedUrl, settings.threshold, lastExecutedAt)
+  Slack.post(settings.slackToken, settings.slackPostChannelId, settings.slackUserName, settings.slackIconEmoji, pageList)
 
   Recorder.record(executedAt)
 }
