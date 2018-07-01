f4d5d503a7b88766d16d4e49931cd2a893f61ff8
Hiroshi Sugawara <>
2018-07-01 20:47:08 +0900

    refactor: RSS から URL を取得する場所を変えるために RSS 全体から URL の一覧を抽出する処理としてまとめる

--- src/main/scala/domain/Extractor.scala
+++ src/main/scala/domain/Extractor.scala
@@ -1,5 +1,7 @@
 package domain
 
+import scala.xml.{Elem, NodeSeq, XML}
+
 /**
   * 抽出する処理を行う
   */
@@ -7,10 +9,29 @@ object Extractor {
   /**
     * URL を抽出する
     *
-    * @param raw 対象の文字列
+    * @param xmlString 抽出対象の XML 形式の文字列
     * @return 抽出結果
     */
-  def extractUrl(raw: String): Either[Unit, String] = {
+  def extractUrl(xmlString: String): Seq[String] = {
+    val xml = XML.loadString(xmlString)
+    getItems(xml).map(_.toString).map(getUrl).collect { case Right(r) => r }
+  }
+
+  /**
+    * 処理対象の一覧を取得する
+    *
+    * @param element 抽出対象の XML
+    * @return 処理対象の一覧
+    */
+  private def getItems(element: Elem): NodeSeq = element \ "channel" \ "items" \ "Seq" \ "li"
+
+  /**
+    * URL を抽出する
+    *
+    * @param raw 抽出対象の文字列
+    * @return 抽出した URL
+    */
+  private def getUrl(raw: String): Either[Unit, String] = {
     val r = """.*rdf:resource="(.*?)".*""".r
     r.findFirstMatchIn(raw).map(_.group(1)).toRight(())
   }
