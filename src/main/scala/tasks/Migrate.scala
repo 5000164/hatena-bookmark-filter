c4a698605cfc17164937e80ebfaf3c55b37f8841
Hiroshi Sugawara <>
2018-07-14 00:09:35 +0900

    feat: マイグレーション時にテーブルコードを自動で生成するようにする

--- src/main/scala/tasks/Migrate.scala
+++ src/main/scala/tasks/Migrate.scala
@@ -1,11 +1,25 @@
 package tasks
 
 import org.flywaydb.core.Flyway
+import slick.codegen.SourceCodeGenerator
 
 /** マイグレートを実行する。 */
 object Migrate extends App {
+  val url = "jdbc:sqlite:./db.db"
+
   val flyway = new Flyway()
-  flyway.setDataSource("jdbc:sqlite:./db.db", "", "")
+  flyway.setDataSource(url, "", "")
   flyway.setLocations(s"filesystem:${System.getProperty("user.dir")}/src/main/resources/db/migration")
   flyway.migrate()
+
+  SourceCodeGenerator.run(
+    profile = "slick.jdbc.SQLiteProfile",
+    jdbcDriver = "org.sqlite.JDBC",
+    url = url,
+    outputDir = "src/main/scala",
+    pkg = "infrastructure",
+    user = None,
+    password = None,
+    ignoreInvalidDefaults = true
+  )
 }
