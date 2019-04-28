3c0415ff21d469844bee4443050c18d794e523fb
Hiroshi Sugawara <5000164@users.noreply.github.com>
2019-04-28 15:30:27 +1000

    fix: to close a resource

--- common/src/main/scala/infrastructure/Settings.scala
+++ common/src/main/scala/infrastructure/Settings.scala
@@ -1,12 +1,14 @@
 package infrastructure
 
-import scala.io.Source
+import scala.io.{BufferedSource, Source}
 import scala.reflect.runtime.{currentMirror, universe}
 import scala.tools.reflect.ToolBox
 
 object Settings {
   val toolbox: ToolBox[universe.type] = currentMirror.mkToolBox()
-  val settings: SettingsType          = toolbox.eval(toolbox.parse(Source.fromFile(System.getProperty("settings")).mkString)).asInstanceOf[SettingsType]
+  val source: BufferedSource          = Source.fromFile(System.getProperty("settings"))
+  val settings: SettingsType          = toolbox.eval(toolbox.parse(source.mkString)).asInstanceOf[SettingsType]
+  source.close()
 }
 
 trait SettingsType {
